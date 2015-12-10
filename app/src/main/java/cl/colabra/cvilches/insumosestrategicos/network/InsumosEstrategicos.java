c5fd123d2487e2cba383fecb2c42edeea0da1189
Carlos Vilches Mandiola <carlos.vilches.mandiola@gmail.com>
2015-12-10 15:25:46 -0300

    Adds Cookie manager.

--- app/src/main/java/cl/colabra/cvilches/insumosestrategicos/network/InsumosEstrategicos.java
+++ app/src/main/java/cl/colabra/cvilches/insumosestrategicos/network/InsumosEstrategicos.java
@@ -1,36 +1,48 @@
 package cl.colabra.cvilches.insumosestrategicos.network;
 
-import android.app.Application;
+import android.content.Context;
 
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.Volley;
 
+import java.net.CookieHandler;
+import java.net.CookieManager;
+
 import cl.colabra.cvilches.insumosestrategicos.utils.NukeSSLCerts;
 
-public class InsumosEstrategicos extends Application {
-
-    private static InsumosEstrategicos sInstance;
-
+public class InsumosEstrategicos {
+    private static InsumosEstrategicos mInstance;
     private RequestQueue mRequestQueue;
+    private static Context mCtx;
 
-    public synchronized static InsumosEstrategicos getInstance() {
-        return sInstance;
+    private InsumosEstrategicos(Context context) {
+        mCtx = context;
+        mRequestQueue = getRequestQueue();
+
+        NukeSSLCerts.nuke();
     }
 
-    @Override
-    public void onCreate() {
-        super.onCreate();
-
-        // This instruction makes the App accept all SSL certificates
-        NukeSSLCerts.nuke();
-
-        mRequestQueue = Volley.newRequestQueue(this);
-
-        sInstance = this;
+    public static synchronized InsumosEstrategicos getInstance(Context context) {
+        if (mInstance == null) {
+            mInstance = new InsumosEstrategicos(context);
+        }
+        return mInstance;
     }
 
     public RequestQueue getRequestQueue() {
+        if (mRequestQueue == null) {
+            CookieManager manager = new CookieManager();
+            CookieHandler.setDefault(manager);
+            // getApplicationContext() is key, it keeps you from leaking the
+            // Activity or BroadcastReceiver if someone passes one in.
+            mRequestQueue = Volley.newRequestQueue(mCtx.getApplicationContext());
+        }
         return mRequestQueue;
+    }
+
+    public <T> void addToRequestQueue(Request<T> req) {
+        getRequestQueue().add(req);
     }
 
 }
