f3bb411b984545a4caf34fa573f265d5783cd036
Carlos Vilches Mandiola <carlos.vilches.mandiola@gmail.com>
2015-12-14 14:18:06 -0300

    Adds icons to tabs.

--- app/src/main/java/cl/colabra/cvilches/insumosestrategicos/MainActivity.java
+++ app/src/main/java/cl/colabra/cvilches/insumosestrategicos/MainActivity.java
@@ -1,23 +1,26 @@
 package cl.colabra.cvilches.insumosestrategicos;
 
+import android.content.Context;
 import android.content.Intent;
-import android.support.design.widget.TabLayout;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.ImageSpan;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-
 import android.widget.TextView;
 
 import cl.colabra.cvilches.insumosestrategicos.utils.SessionManager;
@@ -39,6 +42,9 @@ public class MainActivity extends AppCompatActivity {
      */
     private ViewPager mViewPager;
 
+    /**
+     * The Session Manager class
+     */
     private SessionManager sessionManager;
 
     @Override
@@ -52,7 +58,8 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         // Create the adapter that will return a fragment for each of the three
         // primary sections of the activity.
-        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager(),
+                MainActivity.this);
 
         // Set up the ViewPager with the sections adapter.
         mViewPager = (ViewPager) findViewById(R.id.container);
@@ -60,6 +67,7 @@ public class MainActivity extends AppCompatActivity {
 
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(mViewPager);
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -87,15 +95,22 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        /*if (id == R.id.action_settings) {
             return true;
-        } else if (id == R.id.action_logout) {
+        } else*/
+        if (id == R.id.action_logout) {
             sessionManager.logoutUser();
             startLoginActivity();
             return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void startLoginActivity() {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+        finish();
     }
 
     /**
@@ -139,8 +154,32 @@ public class MainActivity extends AppCompatActivity {
      */
     public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
-        public SectionsPagerAdapter(FragmentManager fm) {
+        // Page Count
+        private static final int PAGE_COUNT = 3;
+
+        private Context context;
+
+        /**
+         * Tab Icons Array
+         */
+        private int imageResId[] = new int[] {
+                R.drawable.ic_daily_plan,
+                R.drawable.ic_register,
+                R.drawable.ic_sync
+        };
+
+        /**
+         * Tab Titles
+         */
+        private String tabNames[] = new String[] {
+                "Plan Diario",
+                "Registro de Stock",
+                "Sync Registros"
+        };
+
+        public SectionsPagerAdapter(FragmentManager fm, Context context) {
             super(fm);
+            this.context = context;
         }
 
         @Override
@@ -152,27 +191,20 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public int getCount() {
-            // Show 3 total pages.
-            return 3;
+            return PAGE_COUNT;
         }
 
         @Override
         public CharSequence getPageTitle(int position) {
-            switch (position) {
-                case 0:
-                    return "Plan Diario";
-                case 1:
-                    return "Registro de Stock";
-                case 2:
-                    return "Sincronización";
-            }
-            return null;
+            // Generate title based on item position
+            Drawable image = context.getResources().getDrawable(imageResId[position]);
+            image.setBounds(0, 0, image.getIntrinsicWidth(), image.getIntrinsicHeight());
+            // Replace blank spaces with image icon
+            SpannableString sb = new SpannableString("   " + tabNames[position]);
+            ImageSpan imageSpan = new ImageSpan(image, ImageSpan.ALIGN_BOTTOM);
+            sb.setSpan(imageSpan, 0, 1, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+            return sb;
         }
-    }
 
-    private void startLoginActivity() {
-        Intent intent = new Intent(this, LoginActivity.class);
-        startActivity(intent);
-        finish();
     }
 }
