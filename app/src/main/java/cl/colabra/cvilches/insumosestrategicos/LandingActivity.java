cad2e5d4450b0618b29282b8c59d5dfd0589e1b3
Carlos Vilches Mandiola <carlos.vilches.mandiola@gmail.com>
2015-12-15 01:06:16 -0300

    Creates DailyPlan Activity.

--- app/src/main/java/cl/colabra/cvilches/insumosestrategicos/LandingActivity.java
+++ app/src/main/java/cl/colabra/cvilches/insumosestrategicos/LandingActivity.java
@@ -7,7 +7,6 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
 import cl.colabra.cvilches.insumosestrategicos.utils.SessionManager;
