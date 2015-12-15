d3a9074d4edd7bb8592d428066f67cf386a16673
Carlos Vilches Mandiola <carlos.vilches.mandiola@gmail.com>
2015-12-15 02:39:55 -0300

    Fix activity backwards navigation.

--- app/src/main/java/cl/colabra/cvilches/insumosestrategicos/LandingActivity.java
+++ app/src/main/java/cl/colabra/cvilches/insumosestrategicos/LandingActivity.java
@@ -79,6 +79,5 @@ public class LandingActivity extends AppCompatActivity {
     private void startDailyPlanActivity() {
         Intent intent = new Intent(this, DailyPlanActivity.class);
         startActivity(intent);
-        finish();
     }
 }
