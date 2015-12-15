5891289fec2e3f106e1714caaa6a861505c1dc27
Carlos Vilches Mandiola <carlos.vilches.mandiola@gmail.com>
2015-12-15 00:59:24 -0300

    Creates LandingActivity which redirects to the other activities within the app.

--- app/src/main/java/cl/colabra/cvilches/insumosestrategicos/LandingActivity.java
+++ app/src/main/java/cl/colabra/cvilches/insumosestrategicos/LandingActivity.java
@@ -6,6 +6,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 import cl.colabra.cvilches.insumosestrategicos.utils.SessionManager;
 
@@ -20,6 +23,31 @@ public class LandingActivity extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        RelativeLayout vDailyPlanLayout = (RelativeLayout) findViewById(R.id.daily_plan);
+        RelativeLayout vStockRegisterLayout = (RelativeLayout) findViewById(R.id.stock_register);
+        RelativeLayout vSyncLayout = (RelativeLayout) findViewById(R.id.sync);
+
+        vDailyPlanLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startDailyPlanActivity();
+            }
+        });
+
+        vStockRegisterLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startDailyPlanActivity();
+            }
+        });
+
+        vSyncLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startDailyPlanActivity();
+            }
+        });
 
         sessionManager = new SessionManager(this);
     }
@@ -45,6 +73,12 @@ public class LandingActivity extends AppCompatActivity {
 
     private void startLoginActivity() {
         Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    private void startDailyPlanActivity() {
+        Intent intent = new Intent(this, LandingActivity.class);
         startActivity(intent);
         finish();
     }
