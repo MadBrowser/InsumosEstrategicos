d81559c4cc3b4b8a814380f60984e3ea4a9c7e4e
Carlos Vilches Mandiola <carlos.vilches.mandiola@gmail.com>
2015-12-14 22:55:36 -0300

    Fragment with list of storehouses.

--- app/src/main/java/cl/colabra/cvilches/insumosestrategicos/fragments/DailyPlanFragment.java
+++ app/src/main/java/cl/colabra/cvilches/insumosestrategicos/fragments/DailyPlanFragment.java
@@ -5,14 +5,22 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.amulyakhare.textdrawable.TextDrawable;
 
 import java.util.List;
 
 import cl.colabra.cvilches.insumosestrategicos.R;
 import cl.colabra.cvilches.insumosestrategicos.model.Storehouse;
+import cl.colabra.cvilches.insumosestrategicos.utils.DividerItemDecoration;
 
 public class DailyPlanFragment extends Fragment {
 
@@ -23,7 +31,8 @@ public class DailyPlanFragment extends Fragment {
     private List<Storehouse> mStorehouses;
 
     // View elements
-    private FloatingActionButton fab;
+    private FloatingActionButton mFab;
+    private RecyclerView mRecyclerView;
 
     public DailyPlanFragment() {
         // Required empty public constructor
@@ -47,11 +56,21 @@ public class DailyPlanFragment extends Fragment {
         View layout = inflater.inflate(R.layout.fragment_daily_plan, container, false);
 
         // Set-up recycler view
-
-
+        mRecyclerView = (RecyclerView) layout.findViewById(R.id.storehouses_list);
+        // Layout Manager
+        LinearLayoutManager lym = new LinearLayoutManager(getActivity());
+        mRecyclerView.setLayoutManager(lym);
+        // Item Decorator
+        RecyclerView.ItemDecoration id = new DividerItemDecoration(getActivity(), LinearLayout.VERTICAL);
+        mRecyclerView.addItemDecoration(id);
+        // Fixed Size
+        mRecyclerView.setHasFixedSize(true);
+        // Adapter
+        DailyPlanAdapter adapter = new DailyPlanAdapter(getActivity(), mStorehouses);
+        mRecyclerView.setAdapter(adapter);
 
         // Floating Action Button
-        fab = (FloatingActionButton) layout.findViewById(R.id.fab);
+        mFab = (FloatingActionButton) layout.findViewById(R.id.fab);
 
         return layout;
     }
@@ -83,6 +102,75 @@ public class DailyPlanFragment extends Fragment {
         void onDailyPlanCreated(Uri uri);
     }
 
+    public class DailyPlanAdapter extends RecyclerView.Adapter<DailyPlanAdapter.DailyPlanViewHolder> {
 
+        private Context context;
+        private List<Storehouse> storehouses;
+        private TextDrawable.IBuilder mDrawableBuilder;
+
+        public DailyPlanAdapter(Context context, List<Storehouse> storehouses) {
+            this.context = context;
+            this.storehouses = storehouses;
+            mDrawableBuilder = TextDrawable.builder().round();
+        }
+
+        @Override
+        public DailyPlanViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            final View DailyPlanView = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.viewholder_storehouse, parent, false);
+            return new DailyPlanViewHolder(DailyPlanView);
+        }
+
+        @Override
+        public void onBindViewHolder(DailyPlanViewHolder holder, int position) {
+            Storehouse storehouse = storehouses.get(position);
+            holder.mStoreDescription.setText(storehouse.getDescription());
+            holder.mLastReading.setText(context.getString(R.string.last_reading,
+                    storehouse.getLastReading()));
+            String stockValue = Float.toString(storehouse.getPercentageStock());
+            int color;
+
+            switch (storehouse.getStockLight()) {
+                case Storehouse.RED_LIGHTS:
+                    color = R.color.stockLightRed;
+                    break;
+                case Storehouse.YELLOW_LIGHTS:
+                    color = R.color.stockLightYellow;
+                    break;
+                case Storehouse.GREEN_LIGHTS:
+                    color = R.color.stockLightGreen;
+                    break;
+                default:
+                    color = R.color.stockLightGreen;
+                    break;
+            }
+            holder.mStockLights.setImageDrawable(mDrawableBuilder.build(stockValue, color));
+        }
+
+        public int getItemCount() {
+            return mStorehouses.size();
+        }
+
+        public class DailyPlanViewHolder extends RecyclerView.ViewHolder implements
+                View.OnClickListener {
+
+            private TextView mStoreDescription;
+            private TextView mLastReading;
+            private ImageView mStockLights;
+
+            public DailyPlanViewHolder(View v) {
+                super(v);
+                this.mStoreDescription = (TextView) v.findViewById(R.id.storehouse_description);
+                this.mLastReading = (TextView) v.findViewById(R.id.last_reading);
+                this.mStockLights = (ImageView) v.findViewById(R.id.stock_light);
+                mStockLights.setOnClickListener(this);
+            }
+
+            @Override
+            public void onClick(View v) {
+
+            }
+        }
+    }
 
 }
