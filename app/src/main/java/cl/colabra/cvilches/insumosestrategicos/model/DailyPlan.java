3f2222af74f5d13d15e7c47279299a50a3273809
Carlos Vilches Mandiola <carlos.vilches.mandiola@gmail.com>
2015-12-20 02:51:57 -0300

    Create models for Daily Plan detail.

---
+++ app/src/main/java/cl/colabra/cvilches/insumosestrategicos/model/DailyPlan.java
@@ -0,0 +1,9 @@
+package cl.colabra.cvilches.insumosestrategicos.model;
+
+/**
+ * Project: InsumosEstrategicos.
+ * Created by Carlos Vilches on 12/17/15. By appointment
+ * of Colabra for client 'Minera Collahuasi'
+ */
+public class DailyPlan {
+}
